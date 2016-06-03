dbcd438b40dec2185ee0722d4e642d50d5c0ee5f
Randy Davis <randy_davis@outlook.com>
2016-06-02 20:42:35 -0500

    add todos.js; completed first requirement;

---
+++ version2_Functions/todos.js
@@ -0,0 +1,18 @@
+/**
+ * Created by randy on 6/2/16.
+ */
+/*
+    REQUIREMENTS:
+    - It should have a function to display todos
+    - It should have a function to add todos
+    - It should have a function to change todos
+    - It should have a function to delete todos
+ */
+
+var todos = ['item 1', 'item 2', 'item 3'];
+
+function displayTodos () {
+    console.log('My todos:', todos);
+}
+
+displayTodos();
