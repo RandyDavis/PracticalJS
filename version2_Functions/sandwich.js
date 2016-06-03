72739e81af4f995ae199600323ec4198df59d346
Randy Davis <randy_davis@outlook.com>
2016-06-02 20:09:59 -0500

    add directory for section 2 - Functions; add sandwich.js showing makeSandwich() recipy analogy

---
+++ version2_Functions/sandwich.js
@@ -0,0 +1,18 @@
+/**
+ * Created by randy on 6/2/16.
+ */
+
+/*
+makeSandwichWith ____
+    Get one slice of bread.
+    Add ___.
+    Put a slice of bread on top.
+*/
+
+function makeSandwichWith(filling) {
+    //Get one slice of bread;
+    //Add filling;
+    //Put a slice of bread on top;
+}
+
+makeSandwichWith(ham);
