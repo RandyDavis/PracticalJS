50cf837b31ee06ce244b75f907e5a3caa958bf1a
Randy Davis <randy_davis@outlook.com>
2016-06-04 23:20:25 -0500

    complete add a addTodo method

--- version3_Objects/object.js
+++ version3_Objects/object.js
@@ -21,5 +21,9 @@ var todoList = {
     todos: ['item 1', ' item 2', 'item 3'],
         displayTodos: function () {
         console.log('My Todos: ', this.todos);
+    },
+    addTodo: function (todo) {
+        this.todos.push(todo);
+        this.displayTodos();
     }
 };
