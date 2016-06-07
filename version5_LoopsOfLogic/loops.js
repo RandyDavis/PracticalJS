2853fa410785f5fab31e84e65bbe4e9170903c5a
Randy Davis <randy_davis@outlook.com>
2016-06-07 00:08:33 -0500

    add displayTodos should show completed section

--- version5_LoopsOfLogic/loops.js
+++ version5_LoopsOfLogic/loops.js
@@ -38,7 +38,11 @@ var todoList = {
         } else {
             console.log('My Todos: ');
             for (var i = 0; i < this.todos.length; i++) {
-                console.log(this.todos[i].todoText);
+                if (this.todos[i].completed) {
+                    console.log('(x)', this.todos[i].todoText);
+                } else {
+                    console.log('( )', this.todos[i].todoText);
+                }
             }
         }
     },
