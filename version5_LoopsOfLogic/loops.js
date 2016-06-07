5abc9ce56f2b1c47fc7c6e26b79848cb435b6fb6
Randy Davis <randy_davis@outlook.com>
2016-06-06 23:27:06 -0500

    add Version 5 directory over Loops; add the for loop example;

---
+++ version5_LoopsOfLogic/loops.js
@@ -0,0 +1,22 @@
+/**
+ * Created by randy on 6/6/16.
+ */
+/*
+ i = 0                 // Initialization
+ Say "hey" if i < 3    // Condition
+ Increase i by 1       // final-expression
+
+ 0 "hey"
+ 1 "hey"
+ 2 "hey"
+ 3
+
+ for(initialization; condition; final-expression) {
+ console.log('Hey');
+ }
+ */
+
+// the for loop
+for (var i = 0; i < 3; i++) {
+  console.log('Hey');
+}
