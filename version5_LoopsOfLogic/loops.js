cc93ef1c0fd84eb638146217f89a6ae2c62e4fbd
Randy Davis <randy_davis@outlook.com>
2016-06-06 23:27:42 -0500

    add looping over arrays example;

--- version5_LoopsOfLogic/loops.js
+++ version5_LoopsOfLogic/loops.js
@@ -17,6 +17,14 @@
  */
 
 // the for loop
-for (var i = 0; i < 3; i++) {
-  console.log('Hey');
+// for (var i = 0; i < 3; i++) {
+//   console.log('Hey');
+// }
+
+
+// looping over arrays
+var testArray = ["item 1", "item 2", "item 3"];
+
+for (var i = 0; i < testArray.length; i++) {
+  console.log(testArray[i]);
 }
