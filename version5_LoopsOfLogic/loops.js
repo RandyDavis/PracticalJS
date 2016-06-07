49e64f9d5cd2b8d872eea7b00709ea5cb02cfc11
Randy Davis <randy_davis@outlook.com>
2016-06-06 23:38:14 -0500

    add displayTodos should show .todoText example;

--- version5_LoopsOfLogic/loops.js
+++ version5_LoopsOfLogic/loops.js
@@ -23,8 +23,47 @@
 
 
 // looping over arrays
-var testArray = ["item 1", "item 2", "item 3"];
+// var testArray = ["item 1", "item 2", "item 3"];
+//
+// for (var i = 0; i < testArray.length; i++) {
+//   console.log(testArray[i]);
+// }
 
-for (var i = 0; i < testArray.length; i++) {
-  console.log(testArray[i]);
+// displayTodos should show .todoText
+var todoList = {
+    todos: [],
+    displayTodos: function () {
+        console.log('My Todos: ');
+        for (var i = 0; i < this.todos.length; i++) {
+            console.log(this.todos[i].todoText);
+        }
+    },
+    addTodo: function (todoText) {
+        this.todos.push({todoText: todoText, complete: false});
+        this.displayTodos();
+    },
+    changeTodo: function (index, todoText) {
+        this.todos[index].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function (index) {
+        this.todos.splice(index, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function (index) {
+        var todo = this.todos[index];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
