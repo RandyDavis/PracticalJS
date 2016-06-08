0e0f50b4256a21eb6b332553ef95d5bb8a026132
Randy Davis <randy_davis@outlook.com>
2016-06-08 00:31:34 -0500

    add .toggleAll make everything true functionality example;

--- version6_ThinkingInCode/toggleAll.js
+++ version6_ThinkingInCode/toggleAll.js
@@ -56,6 +56,12 @@ var todoList = {
                 this.todos[i].completed = false;
             }
         }
+        // Case 2: Otherwise, make everything true
+        else {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;
+            }
+        }
         this.displayTodos();
     }
 }
