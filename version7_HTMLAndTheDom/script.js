5ddfb24a9c4f7dd82507b980ffb41e2f380845a8
Randy Davis <randy_davis@outlook.com>
2016-06-12 03:37:03 -0500

    add button id for display todos button; add working code for the button.

--- version7_HTMLAndTheDom/script.js
+++ version7_HTMLAndTheDom/script.js
@@ -67,3 +67,11 @@ var todoList = {
         this.displayTodos();
     }
 }
+
+// 1. We want to get access to the display todos button.
+var displayTodosButton = document.getElementById("displayTodosButton");
+
+// 2. We want to run displayTodos method, when someone clicks the display todos button.
+displayTodosButton.addEventListener("click", function () {
+    todoList.displayTodos();
+});
