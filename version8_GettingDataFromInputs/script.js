fff6444e8bf6c663fa3f5a9f408ac487033910fe
Randy Davis <randy_davis@outlook.com>
2016-06-19 02:09:32 -0500

    add input and button for toggleCompleted todo;

--- version8_GettingDataFromInputs/script.js
+++ version8_GettingDataFromInputs/script.js
@@ -71,9 +71,6 @@ var handlers = {
     displayTodos: function () {
         todoList.displayTodos();
     },
-    toggleAll: function () {
-        todoList.toggleAll();
-    },
     addTodo: function () {
         var addTodoTextInput = document.getElementById("addTodoTextInput");
         todoList.addTodo(addTodoTextInput.value);
@@ -92,6 +89,15 @@ var handlers = {
 
         todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
         deleteTodoPositionInput.value = '';
+    },
+    toggleCompleted: function () {
+        var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = '';
+    },
+    toggleAll: function () {
+        todoList.toggleAll();
     }
 };
 
