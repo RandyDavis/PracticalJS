e840c343d019afae861b0f66d885fdc24e62058b
Randy Davis <randy_davis@outlook.com>
2016-06-19 04:11:07 -0500

    cleanup/refactor code to get rid of all function calls to displayTodo in the console as everything is now connected to the DOM

--- version9_EscapeFromTheConsole/script.js
+++ version9_EscapeFromTheConsole/script.js
@@ -6,36 +6,18 @@
 
 var todoList = {
     todos: [],
-    displayTodos: function () {
-        if (this.todos.length === 0) {
-            console.log("Your todo list is empty!");
-        } else {
-            console.log('My Todos: ');
-            for (var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].completed) {
-                    console.log('(x) ', this.todos[i].todoText);
-                } else {
-                    console.log('( )', this.todos[i].todoText);
-                }
-            }
-        }
-    },
     addTodo: function (todoText) {
         this.todos.push({todoText: todoText, completed: false});
-        this.displayTodos();
     },
     changeTodo: function (index, todoText) {
         this.todos[index].todoText = todoText;
-        this.displayTodos();
     },
     deleteTodo: function (index) {
         this.todos.splice(index, 1);
-        this.displayTodos();
     },
     toggleCompleted: function (index) {
         var todo = this.todos[index];
         todo.completed = !todo.completed;
-        this.displayTodos();
     },
     toggleAll: function () {
         var totalTodos = this.todos.length;
@@ -47,33 +29,27 @@ var todoList = {
             }
         }
 
-        // Get number of completed todos.
         for (var i = 0; i < totalTodos; i++) {
             if (this.todos[i].completed) {
                 completedTodos++;
             }
         }
 
-        // Case 1: If everything's true, make everything false
         if (completedTodos === totalTodos) {
             toggle(false);
         }
-        // Case 2: Otherwise, make everything true
         else {
             toggle(true);
         }
-        this.displayTodos();
     }
-}
+};
 
 var handlers = {
-    displayTodos: function () {
-        todoList.displayTodos();
-    },
     addTodo: function () {
         var addTodoTextInput = document.getElementById("addTodoTextInput");
         todoList.addTodo(addTodoTextInput.value);
         addTodoTextInput.value = "";
+        view.displayTodos();
     },
     changeTodo: function () {
         var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
@@ -82,21 +58,25 @@ var handlers = {
         todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
         changeTodoPositionInput.value = '';
         changeTodoTextInput.value = '';
+        view.displayTodos();
     },
     deleteTodo: function () {
         var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
 
         todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
         deleteTodoPositionInput.value = '';
+        view.displayTodos();
     },
     toggleCompleted: function () {
         var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
 
         todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
         toggleCompletedPositionInput.value = '';
+        view.displayTodos();
     },
     toggleAll: function () {
         todoList.toggleAll();
+        view.displayTodos();
     }
 };
 
