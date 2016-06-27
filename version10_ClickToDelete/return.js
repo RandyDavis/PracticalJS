db4978262089e7295fac976d5449df5beaf8cea9
Randy Davis <randy_davis@outlook.com>
2016-06-26 23:41:54 -0500

    add version10_ClickToDelete section; completed return statement section;

---
+++ version10_ClickToDelete/return.js
@@ -0,0 +1,10 @@
+/**
+ * Created by randy on 6/26/16.
+ */
+function multiplyTwoNumbers (a, b) {
+    return a * b;
+}
+
+var theProductOf2And10 = multiplyTwoNumbers(2, 10);
+
+theProductOf2And10;  // 20
