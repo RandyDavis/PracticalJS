1cae449e386efe236c76288d3060077f29ee82f4
Randy Davis <randy_davis@outlook.com>
2016-06-27 00:08:48 -0500

    update index file to include todoList app markup; add todoList js to script.js file; complete first requirement of creating delete buttons;

---
+++ version10_ClickToDelete/script.js
@@ -0,0 +1,125 @@
+/**
+ * Created by randy on 6/26/16.
+ */
+// V10 REQUIREMENTS
+/*
+- There should be a way to create delete buttons
+- There should be a delete button for each todo
+- Each li should have an id that has the todo position
+- Delete buttons should have access to the todo id
+- Clicking delete should update todoList.todos and the DOM
+ */
+
+var todoList = {
+    todos: [],
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+    },
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+    },
+    toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+
+        // Get number of completed todos.
+        for (var i = 0; i < totalTodos; i++) {
+            if (this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+
+        // Case 1: If everything’s true, make everything false.
+        if (completedTodos === totalTodos) {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+            // Case 2: Otherwise, make everything true.
+        } else {
+            for (var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;
+            }
+        }
+    }
+};
+
+var handlers = {
+    addTodo: function() {
+        var addTodoTextInput = document.getElementById('addTodoTextInput');
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = '';
+        view.displayTodos();
+    },
+    changeTodo: function() {
+        var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+        var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = '';
+        changeTodoTextInput.value = '';
+        view.displayTodos();
+    },
+    deleteTodo: function() {
+        var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = '';
+        view.displayTodos();
+    },
+    toggleCompleted: function() {
+        var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = '';
+        view.displayTodos();
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
+        view.displayTodos();
+    }
+};
+
+var view = {
+    displayTodos: function() {
+        var todosUl = document.querySelector('ul');
+        todosUl.innerHTML = '';
+        for (var i = 0; i < todoList.todos.length; i++) {
+            var todoLi = document.createElement('li');
+            var todo = todoList.todos[i];
+            var todoTextWithCompletion = '';
+
+            if (todo.completed === true) {
+                todoTextWithCompletion = '(x) ' + todo.todoText;
+            } else {
+                todoTextWithCompletion = '( ) ' + todo.todoText;
+            }
+
+            todoLi.textContent = todoTextWithCompletion;
+            todosUl.appendChild(todoLi);
+        }
+    },
+    createDeleteButton: function () {
+        var deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'deleteButton';
+        return deleteButton;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
